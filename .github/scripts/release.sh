905fc91e80514c864c0009cf371c7f564ac2a468
Sibin Grasic <sibin.grasic@oblak.studio>
2021-12-18 21:18:48 +0100

    chore(build): Tuning WP deploy process

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -5,7 +5,7 @@ NEXT_VERSION=$1
 CURRENT_VERSION=$(cat $PROJECT_NAME.php | grep "Version" | head -1 | awk -F= "{ print $2 }" | sed 's/[* Version:,\",]//g' | tr -d '[[:space:]]')
 
 sed -i "s/Version:              $CURRENT_VERSION/Version:              $NEXT_VERSION/g" serbian-addons-for-woocommerce.php
-sed -i "/Stable tag: $CURRENT_VERSION/Stable tag: $NEXT_VERSION/g" .wordpress-org/readme.txt
+sed -i "s/Stable tag: $CURRENT_VERSION/Stable tag: $NEXT_VERSION/g" ./.wordpress-org/readme.txt
 
 rm -f /tmp/release.zip
 mkdir /tmp/$PROJECT_NAME
--- serbian-addons-for-woocommerce.php
+++ serbian-addons-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name:          Serbian Addons for WooCommerce
  * Plugin URI:           https://oblak.studio/open-source/srpski-woocommerce
  * Description:          Various addons and tweaks that make WooCommerce compatible with Serbian bureaucracy.
- * Version:              1.1.0
+ * Version:              1.0.0
  * Requires PHP:         7.3
  * Author:               Oblak Studio
  * Author URI:           https://oblak.studio
