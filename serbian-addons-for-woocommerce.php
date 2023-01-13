98f188a7dfc85ed108a49749386f7588c1e517f9
Sibin Grasic <sibin.grasic@oblak.studio>
2023-01-13 19:18:57 +0100

    feat: Added support for HPOS

--- serbian-addons-for-woocommerce.php
+++ serbian-addons-for-woocommerce.php
@@ -25,6 +25,12 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/lib/Utils/core.php';
 require __DIR__ . '/lib/Utils/helpers.php';
 
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
+
 add_action(
     'woocommerce_loaded',
     function() {
