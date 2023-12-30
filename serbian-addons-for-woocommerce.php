8cdf65ce58ec39f76306edf605b2438894ddffc1
Sibin Grasic <sibin.grasic@oblak.studio>
2023-12-30 22:19:22 +0100

    fix: Fixed PHP required version

--- serbian-addons-for-woocommerce.php
+++ serbian-addons-for-woocommerce.php
@@ -4,12 +4,12 @@
  * Plugin URI:           https://oblak.studio/open-source/srpski-woocommerce
  * Description:          Various addons and tweaks that make WooCommerce compatible with Serbian bureaucracy.
  * Version:              3.2.1
- * Requires PHP:         7.4
+ * Requires PHP:         8.0
  * Author:               Oblak Studio
  * Author URI:           https://oblak.studio
  * Tested up to:         6.3
- * WC requires at least: 7.0
- * WC tested up to:      8.0
+ * WC requires at least: 8.0
+ * WC tested up to:      8.3
  * License:              GPLv2 or later
  * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:          serbian-addons-for-woocommerce
