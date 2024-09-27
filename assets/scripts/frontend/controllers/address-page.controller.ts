39b533605f1ebc34f745c8e2edf7d4161736b1ea
Sibin Grasic <sibin.grasic@oblak.studio>
2024-09-27 12:10:09 +0200

    fix(Checkout): Field toggle

--- assets/scripts/frontend/controllers/address-page.controller.ts
+++ assets/scripts/frontend/controllers/address-page.controller.ts
@@ -1,28 +1,29 @@
 const $ = jQuery;
 
 export default class AddressPageController {
-  private selector = '.entity-type-control input';
-  private locale: Record<string, string>;
+  private field = '';
+  private labels: Record<string, string>;
 
   public init(): void {
-    this.locale = JSON.parse(window.wc_address_i18n_params.locale_fields);
+    this.labels = {
+      reqText: window.wc_address_i18n_params.i18n_required_text,
+      optText: window.wc_address_i18n_params.i18n_optional_text,
+    };
   }
 
   public finalize(): void {
-    $(document.body).on('change refresh', this.selector, (e) => {
-      this.toggleEntityType($(e.target));
+    $(document.body).on('change refresh', '.entity-type-control input', ({ target }) => {
+      this.toggleFields($(target));
     });
 
     $(document.body).on('country_to_state_changing', () => {
-      window.setTimeout(
-        () => this.toggleEntityType($(`${this.selector}:checked`)),
-        100,
-      );
+      window.setTimeout(() => this.toggleFields($('.entity-type-control input:checked')), 100);
     });
   }
 
-  private toggleEntityType($field: JQuery<HTMLInputElement>): void {
-    const required = $field.val() === 'company';
+  private toggleFields($input: JQuery<HTMLInputElement>): void {
+    const required = $input.val() === 'company';
+    const $field = $<HTMLParagraphElement>('.entity-type-toggle');
 
     $field.find('input').prop({
       'aria-required': required,
@@ -34,27 +35,19 @@ export default class AddressPageController {
       $field.addClass('shown validate-required');
 
       if ($field.find('label .required').length === 0) {
-        $field
-          .find('label')
-          .append(
-            `<abbr class="required" title="${window.wc_address_i18n_params.i18n_required_text}">*</abbr>`,
-          );
+        $field.find('label').append(`<abbr class="required" title="${this.labels.reqText}">*</abbr>`);
       }
 
       return;
     }
 
     $field.find('label .required').remove();
-    $field.removeClass(
-      'shown validate-required woocommerce-invalid woocommerce-invalid-required-field',
-    );
+    $field.removeClass('shown validate-required woocommerce-invalid woocommerce-invalid-required-field');
 
     if ($field.find('label .optional').length === 0) {
       $field
         .find('label')
-        .append(
-          `<abbr class="optional" title="${window.wc_address_i18n_params.i18n_optional_text}">${window.wc_address_i18n_params.i18n_optional_text}</abbr>`,
-        );
+        .append(`<abbr class="optional" title="${this.labels.optText}">${this.labels.optText}</abbr>`);
     }
   }
 }
